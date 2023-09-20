c3bb92a558d24b8d9975443c6289265db0474a96
hhgjf <nusgjf@gmail.com>
2023-09-20 10:44:15 +0800

    change to ecc

--- install.sh
+++ install.sh
@@ -130,17 +130,16 @@ EOF
 CreateCert(){
     $systemPackage -y install socat
     curl https://get.acme.sh | sh
-    ~/.acme.sh/acme.sh  --register-account  -m nusgjf@gmail.com --server letsencrypt
-    ~/.acme.sh/acme.sh  --set-default-ca --server letsencrypt
+    ~/.acme.sh/acme.sh  --register-account  -m nusgjf@gmail.com --server zerossl
     ~/.acme.sh/acme.sh  --issue  -d $yourDomain  --webroot /usr/share/nginx/html/
-    if test -s /root/.acme.sh/$yourDomain/fullchain.cer; then
+    if test -s /root/.acme.sh/"${yourDomain}_ecc"/fullchain.cer; then
         EchoG "[5]申请证书成功."
     else
         EchoR "[error]申请证书失败，开始尝试使用standalone模式申请。"
         systemctl stop nginx
         ~/.acme.sh/acme.sh  --issue  -d $yourDomain  --standalone
         systemctl start nginx
-        if test -s /root/.acme.sh/$yourDomain/fullchain.cer; then
+        if test -s /root/.acme.sh/"${yourDomain}_ecc"/fullchain.cer; then
             EchoG "[info]standalone模式申请证书成功."
         else
             EchoR "[error]standalone模式申请证书失败，请稍后自行申请并相应命名，置于以下路径："
@@ -267,7 +266,7 @@ EOF
     systemctl enable xray.service
     sed -i "s/User=nobody/User=root/;" /etc/systemd/system/xray.service
     systemctl daemon-reload
-    ~/.acme.sh/acme.sh  --installcert  -d  $yourDomain   \
+    ~/.acme.sh/acme.sh  --installcert --ecc  -d  $yourDomain   \
         --key-file   /usr/local/etc/xray/cert/private.key \
         --fullchain-file  /usr/local/etc/xray/cert/fullchain.cer \
         --reloadcmd  "chmod -R 777 /usr/local/etc/xray/cert"
